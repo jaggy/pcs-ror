2640e705d42be60e2bd04eaff48102a7479670c2
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-11 19:15:40 +0800

    prevented null role names to be saved on the database

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,7 +5,9 @@ describe Role do
 	it "has a valid factory" do
 		FactoryGirl.create(:role).should be_valid
 	end
-	it "is invalid without a role name"
+	it "is invalid without a role name" do
+		FactoryGirl.build(:role, name: nil).should_not be_valid
+	end
 	it "doesn't allow duplicate role names"
 
 end
