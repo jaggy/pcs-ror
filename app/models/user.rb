5b91cca002218b9ab880431bf9e840b35f8439e1
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-12 00:17:19 +0800

    presence check tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 	belongs_to :role
   attr_accessible :email, :first_name, :image, :last_name, :middle_name, :password, :status, :username
 	validates :username, :presence => true
+	validates :password, :presence => true
+	validates :first_name, :presence => true
+	validates :last_name, :presence => true
+	validates :email, :presence => true
+	validates :role, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,13 +9,35 @@ describe User do
 	it "is invalid without a username" do
 		FactoryGirl.build(:user, :username => nil).should_not be_valid
 	end
-	it "is invalid without a password"
-	it "is invalid without a first name"
-	it "is valid without a middle name"
-	it "is invalid without a last name"
-	it "is invalid without an email"
-	it "is valid without an image"
-	it "is invalid without a role"
+
+	it "is invalid without a password" do
+		FactoryGirl.build(:user, :password => nil).should_not be_valid
+	end
+
+	it "is invalid without a first name" do
+		FactoryGirl.build(:user, :first_name => nil).should_not be_valid
+	end
+
+	it "is valid without a middle name" do
+		FactoryGirl.build(:user, :middle_name => nil).should be_valid
+	end
+
+	it "is invalid without a last name" do
+		FactoryGirl.build(:user, :last_name => nil).should_not be_valid
+	end
+
+	it "is invalid without an email" do
+		FactoryGirl.build(:user, :email => nil).should_not be_valid
+	end
+
+	it "is valid without an image" do
+		FactoryGirl.build(:user, :image => nil).should be_valid
+	end
+
+	it "is invalid without a role" do
+		FactoryGirl.build(:user, :role => nil).should_not be_valid
+	end
+
 	it "is should has the password before saving"
 	it "doesn't accept duplicate usernames"
 	it "doesn't accept dumplicate emails"
