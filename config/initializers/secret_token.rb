4dfcc86608aee389f92fde6ef3f227a5657731a1
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-11 17:11:29 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PcsGit::Application.config.secret_token = '34ad14e85390b7fb1c898ab0b051363624ed42443577d242266bf71bf997455a1c676c4e9bc355c0208d446b4e87eca81151e25ee46e1051d0bf2324f1771d84'
