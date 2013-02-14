c2811e0182553297479e19aa6138a536d6c033a7
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-14 23:03:22 +0800

    unique post per committee

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -2,4 +2,17 @@ class Discussion < ActiveRecord::Base
   belongs_to :committee
   belongs_to :user
   attr_accessible :description, :title
+
+	validates :title, :presence => true
+	validates :user, :presence => true
+	validates :committee, :presence => true
+
+	validate :unique_committee_post
+
+	def unique_committee_post
+		if self.committee && self.new_record? && Discussion.where(:title => self.title).present?
+			errors.add(:title, 'Title has already been taken')
+		end
+	end
+
 end
--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -6,10 +6,30 @@ describe Discussion do
 		FactoryGirl.create(:discussion).should be_valid
 	end
 
-	it "is invalid without a title"
-	it "is valid without a description"
-	it "is invalid without an owner"
-	it "is invalid without a committee"
-	it "does not allow duplicate titles"
+	it "is invalid without a title"	do
+		FactoryGirl.build(:discussion, :title => nil).should_not be_valid
+	end
+
+	it "is valid without a description" do
+		FactoryGirl.build(:discussion, :description => nil).should be_valid
+	end
+
+	it "is invalid without an owner" do
+		FactoryGirl.build(:discussion, :user => nil).should_not be_valid
+	end
+
+	it "is invalid without a committee" do
+		FactoryGirl.build(:discussion, :committee => nil).should_not be_valid
+	end
+
+	it "does not allow duplicate titles of the same committee" do
+		committee = FactoryGirl.create(:committee)
+		discussion = FactoryGirl.create(:discussion, :committee => committee)
+		FactoryGirl.build(:discussion, :committee => committee, :title => discussion.title).should_not be_valid
+	end
+
+	it "allows the same title for different committees" do
+
+	end
 
 end
