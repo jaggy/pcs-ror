60b74f2d7a65767744d306189c1a87b800d4f10c
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-14 18:36:54 +0800

    added basic validation tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,7 +6,17 @@ describe Category do
 		FactoryGirl.create(:category).should be_valid
 	end
 
-	it "is invalid without a name"
-	it "is valid without a description"
+	it "is invalid without a name" do
+		FactoryGirl.build(:category, :name => nil).should_not be_valid
+	end
+
+	it "is does not accept duplicate names" do
+		category = FactoryGirl.create(:category)
+		FactoryGirl.build(:category, :name => category.name).should_not be_valid
+	end
+
+	it "is valid without a description" do
+		FactoryGirl.build(:category, :description => nil).should be_valid
+	end
 
 end
