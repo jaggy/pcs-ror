fc59b8f0acefc0d79b87f9ee47321ba4c916fa32
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-15 17:27:30 +0800

    had a hard time thinking about their damn tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,10 +6,6 @@ describe Post do
 		FactoryGirl.create(:post).should be_valid
 	end
 
-	it "is invalid without a title" do
-		FactoryGirl.build(:post, :title => nil).should_not be_valid
-	end
-
 	it "is invalid without an owner" do
 		FactoryGirl.build(:post, :user => nil).should_not be_valid
 	end
@@ -30,8 +26,25 @@ describe Post do
 		FactoryGirl.build(:post, :discussion => nil).should be_valid
 	end
 
-	it "does not accept duplicate titles"
-	it "cannot be it's own parent"
-	
+	it "is invalid without a title if unless from a discussion" do
+		FactoryGirl.build(:post, :title => nil, :discussion => nil).should_not be_valid
+	end
+
+	it "is invalid with a title if it's from a discussion" do
+		discussion = FactoryGirl.create(:discussion)
+		FactoryGirl.build(:post, :title => 'should not be valid', :discussion => discussion).should_not be_valid
+	end
+
+	it "does not accept duplicate titles if unless from a discussion" do
+		post = FactoryGirl.create(:post)
+		FactoryGirl.build(:post, :title => post.title).should_not be_valid
+	end
+
+	it "accepts duplicate titles if it's not from a discussion" do
+		discussion = FactoryGirl.create(:discussion)
+		post = FactoryGirl.create(:post, :title => nil, :discussion => discussion)
+		FactoryGirl.build(:post, :title => post.title,  :discussion => discussion).should be_valid
+	end
+
 
 end
