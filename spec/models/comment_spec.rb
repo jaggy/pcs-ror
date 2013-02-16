b8bcc613a5ec6080ad7f39bd4689b826b7ac4156
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-16 08:10:55 +0800

    basic spec and validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "has a valid factory" do
+		FactoryGirl.create(:comment).should be_valid
+	end
+
+	it "is invalid wihtout content" do
+		FactoryGirl.build(:comment, :content => nil).should_not be_valid
+	end
+
+	it "is invalid without an owner"
+	it "is invalid without a post"
+
 end
