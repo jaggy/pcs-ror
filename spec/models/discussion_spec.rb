453dd065170f2fedadc62f3ba4eb08effac1cc8c
Jaggy Gauran <jaggygauran@gmail.com>
2013-02-17 13:29:10 +0800

    cleaned up the discussion specs

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -22,17 +22,26 @@ describe Discussion do
 		FactoryGirl.build(:discussion, :committee => nil).should_not be_valid
 	end
 
-	it "does not allow duplicate titles of the same committee" do
-		committee = FactoryGirl.create(:committee)
-		discussion = FactoryGirl.create(:discussion, :committee => committee)
-		FactoryGirl.build(:discussion, :committee => committee, :title => discussion.title).should_not be_valid
-	end
+	describe "duplicate committees" do
+		
+		before :each do
+			@committee = FactoryGirl.create(:committee)
+			@discussion = FactoryGirl.create(:discussion, :committee => @committee)
+		end
 
-	it "allows the same title for different committees" do
-		committee = FactoryGirl.create(:committee)
-		different_committee = FactoryGirl.create(:committee, :name => 'Different Committee Name')
-		discussion = FactoryGirl.create(:discussion, :committee => committee)
-		FactoryGirl.build(:discussion, :committee => different_committee, :title => discussion.title).should be_valid
+		context "from the same committee" do
+			it "is not valid" do
+				FactoryGirl.build(:discussion, :committee => @committee, :title => @discussion.title).should_not be_valid
+			end
+		end
+
+		context "from different committees" do
+			it "is valid" do
+				different_committee = FactoryGirl.create(:committee, :name => 'Different Committee Name')
+				FactoryGirl.build(:discussion, :committee => different_committee, :title => @discussion.title).should be_valid
+			end
+		end
+
 	end
 
 end
